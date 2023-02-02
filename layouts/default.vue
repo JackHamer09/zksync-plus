64d5aa8113c4866daadc631820e6e0577d34712a
Jack Hamer <jackhamer0990@gmail.com>
2023-02-02 23:15:28 +0200

    fix: layout, add icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="app-layout">
-    <main>
-      <Sidebar />
+    <Sidebar />
+    <main class="app-layout-main">
       <slot />
     </main>
   </div>
@@ -56,6 +56,13 @@
     grid-template-columns:
       var(--rui-layout-start) var(--rui-layout-nav-size) var(--rui-layout-nav-gap) var(--rui-layout-main-size)
       var(--rui-layout-side-gap) var(--rui-layout-side-size) var(--rui-layout-end, 0);
+  }
+
+  .app-layout-main {
+    display: flex;
+    flex-direction: column;
+    @apply flex flex-col px-4 py-2 md:px-0 md:py-4;
+    grid-area: main / main / main / main;
   }
 }
 </style>
