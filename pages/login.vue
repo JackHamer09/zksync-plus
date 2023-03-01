3217b303085175b6761a43df44f8ce2d815650cb
Jack Hamer <jackhamer0990@gmail.com>
2023-03-01 17:42:06 +0200

    fix: better login ui and onboard

--- pages/login.vue
+++ pages/login.vue
@@ -1,37 +1,44 @@
 <template>
   <div class="login-page">
     <h1 class="h1 mb-2 text-center">Log in to zkSync Plus</h1>
-    <button @click="connect" class="login-btn">
-      <IconsMetaMaskAnimated svg-classes="h-12 w-12 h-16 md:w-16" class="mr-2 h-12 w-auto md:h-16" />
-      <div>
+    <button @click="openModal" class="login-btn">
+      <div class="login-btn-inner">
+        <IconsEthereum class="mr-2 h-14 w-auto md:h-20" />
         <div class="login-btn-description">Connect your Ethereum wallet to experience zkSync Plus</div>
+        <ChevronRightIcon class="h-8 w-8" aria-hidden="true" />
       </div>
     </button>
   </div>
 </template>
 
 <script lang="ts" setup>
-import useOnboard from "@/composables/useOnboard";
+import { ChevronRightIcon } from "@heroicons/vue/24/solid";
 
 import { definePageMeta } from "#imports";
+import { openModal } from "@/store/useOnboard";
 
 definePageMeta({
   layout: "login",
 });
-
-const { connectWallet } = useOnboard();
-
-function connect() {
-  connectWallet();
-}
 </script>
 
 <style lang="scss" scoped>
 .login-page {
   .login-btn {
-    @apply mt-5 flex w-full items-center justify-between gap-2.5 rounded-lg border bg-white py-3 px-5 pr-6 text-left shadow-sm transition-shadow hover:shadow-md disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none disabled:hover:shadow-none;
+    @apply mt-5 w-full rounded-lg bg-white p-1 text-left text-gray-secondary transition-shadow disabled:cursor-not-allowed disabled:opacity-50 disabled:shadow-none;
+    &:hover:not(:disabled) {
+      @apply shadow-sm;
+
+      .login-btn-inner {
+        @apply bg-gray-50;
+      }
+    }
+
+    .login-btn-inner {
+      @apply flex w-full items-center justify-between gap-2.5 rounded-lg px-5 pr-6 transition-colors;
+    }
     .login-btn-description {
-      @apply text-sm font-light leading-tight text-gray-secondary;
+      @apply text-sm font-light leading-tight;
     }
   }
 }
