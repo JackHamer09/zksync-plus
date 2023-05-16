c548e7a6a34f3943a321b3d85e6fd798e2769d97
Jack Hamer <jackhamer0990@gmail.com>
2023-05-16 14:35:44 +0300

    feat: add login page footer

--- views/Login.vue
+++ views/Login.vue
@@ -75,6 +75,33 @@
         Use another account
       </CommonLabelButton>
     </template>
+
+    <footer class="login-footer">
+      <div></div>
+      <div class="made-with-love">
+        Made with ❤️ by
+        <a class="link" href="https://github.com/JackHamer09" target="_blank" rel="noopener">JackHamer</a>
+        and
+        <a class="link" href="https://github.com/JackHamer09/zksync-plus" target="_blank" rel="noopener"
+          >contributors</a
+        >
+      </div>
+      <a
+        href="https://github.com/JackHamer09/zksync-plus"
+        title="zkSync Plus GitHub page"
+        target="_blank"
+        rel="noopener"
+        class="block w-max text-gray-500 hover:text-gray-900"
+      >
+        <svg class="h-7 w-7" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
+          <path
+            fill-rule="evenodd"
+            d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+      </a>
+    </footer>
   </div>
 </template>
 
@@ -117,6 +144,13 @@ const useAnotherAccount = async () => {
     }
     .logged-in-address {
       @apply text-left font-medium text-gray-700;
+    }
+  }
+  .login-footer {
+    @apply absolute left-0 bottom-0 grid w-full items-center justify-items-center gap-3 px-6 pb-5 sm:grid-cols-[1.75rem_1fr_1.75rem];
+
+    .made-with-love {
+      @apply whitespace-pre-line text-center text-sm text-gray-400;
     }
   }
 }
